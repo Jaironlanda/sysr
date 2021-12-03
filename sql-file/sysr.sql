a4f087dd8b158e0398ea2bf7a442f5e27dc011b7
Jairon Landa <jaironlanda@yahoo.com>
2021-12-03 21:30:13 +0800

    update sql file

--- sql-file/sysr.sql
+++ sql-file/sysr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 26, 2021 at 04:44 PM
+-- Generation Time: Dec 03, 2021 at 02:27 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `feedback` (
   `id` int(11) NOT NULL,
   `content` longtext NOT NULL,
-  `report` int(11) NOT NULL,
+  `report_id` int(11) NOT NULL,
   `admin_id` int(11) NOT NULL,
   `datetime_created` datetime NOT NULL,
   `datetime_updated` datetime NOT NULL DEFAULT current_timestamp()
@@ -64,6 +64,26 @@ CREATE TABLE `reportType` (
   `datetime_created` datetime NOT NULL,
   `datetime_updated` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reportType`
+--
+
+INSERT INTO `reportType` (`id`, `name`, `datetime_created`, `datetime_updated`) VALUES
+(1, 'Sedition', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(2, 'Threats to National Security', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(3, 'Fraud/Finance/Trade', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(4, 'Copyright', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(5, 'Defamation ', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(6, 'Insulting the Religion', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(7, 'Immoral/Indecent/\r\nOffensive/Fraud', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(8, 'Identity Theft', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(9, 'Medicines Advertisement/ Sale', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(10, 'Distribution of destructive weapons', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(11, 'Gaming/Betting', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(12, 'Threats to life/property', '2021-11-26 18:22:48', '2021-11-27 01:27:11'),
+(13, 'Hacking', '2021-11-26 18:27:45', '2021-11-27 01:28:00'),
+(14, 'Personal Data Usage', '2021-11-26 18:27:45', '2021-11-27 01:28:00');
 
 -- --------------------------------------------------------
 
@@ -129,7 +149,7 @@ ALTER TABLE `report`
 -- AUTO_INCREMENT for table `reportType`
 --
 ALTER TABLE `reportType`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `user`
